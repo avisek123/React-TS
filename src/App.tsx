b21ecffc6d42543adae01b7a82710d2e22aa3f42
avisek123 <mrperfectavi89@gmail.com>
2021-12-12 01:33:41 +0530

    interface use

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Profile } from './components'
 function App() {
   return (
     <div>
-      <Profile name='Avisek Sahoo'/>
+      <Profile name='Avisek Sahoo' age={12} />
     </div>
   )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 interface profileProps {
     name: string
+    age: number
  
 }
 function Profile(props: profileProps) {
     return (
         <div>
-            I Am {props.name} 
+            I Am {props.name} , {props.age} years old
         </div>
     )
 }
